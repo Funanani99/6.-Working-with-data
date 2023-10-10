866bc0e6bcc88b9b06c69cae37ad27e92a946936
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 08:31:07 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMassage As String
+        Dim BirthYear As Integer
+
+        YourMassage = "Happy birthday! How old are you now"
+
+        MsgBox(YourMassage)
+        BirthYear = Val(InputBox("Enter your birth year"))
+        MsgBox("You are now  " & 2023 - BirthYear & " years old")
+    End Sub
+End Class
